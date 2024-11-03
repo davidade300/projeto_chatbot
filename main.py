6ea7aa0d1b37869f381647b470d6b32a31670566
davidade300 <iaderinho@icloud.com>
2024-11-02 21:22:21 -0300

    lib rich adicionada para renderizar markdown no console

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from openai import OpenAI
 import os
+from rich.console import Console
+from rich.markdown import Markdown
 
 openai = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
-response = openai.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[{
-        "role": "system",
-        "content": "you are a helpful assistant,"
-                   + " you will have conversations in brazilian portuguese and you will not hallucinate"
-    }, {"role": "user",
-        "content": "você entende português?"
-        }],
-)
+chat_log = []
 
-print(response.choices[0].message.content)
+console = Console()
+
+print("digite 'sair' para  sair")
+while True:
+
+    user_input = input("usuario: ")
+
+    if user_input.lower() == "sair":
+        break
+
+    chat_log.append({"role": "user", "content": user_input})
+
+    response = openai.chat.completions.create(
+        model="gpt-3.5-turbo",
+        temperature=0.7,
+        messages=chat_log,
+    )
+
+    bot_response = response.choices[0].message.content
+    chat_log.append({"role": "assistant", "content": bot_response})
+    console.print(Markdown(bot_response))
