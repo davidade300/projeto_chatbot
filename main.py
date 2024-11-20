0a9d89adf5769868da0a6e01fcefc4013aceda16
davidade300 <iaderinho@icloud.com>
2024-11-20 19:51:01 -0300

    branch main resetado para app de console'

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
-from openai import OpenAI
 import os
+from typing import Annotated
+
+from fastapi import FastAPI, Form
+from openai import OpenAI
 from rich.console import Console
-from rich.markdown import Markdown
+
+from prompts.py_instructor import py_instructor_prompt
+
+app = FastAPI()
 
 openai = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
-chat_log = []
+chat_log = [{"role": "system", "content": py_instructor_prompt}]
 
 console = Console()
 
-print("digite 'sair' para  sair")
-while True:
 
-    user_input = input("usuario: ")
-
-    if user_input.lower() == "sair":
-        break
-
+@app.post("/")
+async def chat(user_input: Annotated[str, Form()]):
     chat_log.append({"role": "user", "content": user_input})
 
     response = openai.chat.completions.create(
@@ -27,4 +28,5 @@ while True:
 
     bot_response = response.choices[0].message.content
     chat_log.append({"role": "assistant", "content": bot_response})
-    console.print(Markdown(bot_response))
+    return bot_response
+    # console.print(Markdown(bot_response))
